21cd1ddb4cb0d86057deb91428c33177a2a2d813
hendisantika <hendisantika@yahoo.co.id>
2024-05-15 09:56:52 +0700

    Create Spring Boot App 🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/springbootliquibaseexample/SpringBootLiquibaseExampleApplication.java
@@ -0,0 +1,13 @@
+package id.my.hendisantika.springbootliquibaseexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootLiquibaseExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootLiquibaseExampleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/id/my/hendisantika/springbootliquibaseexample/SpringBootLiquibaseExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package id.my.hendisantika.springbootliquibaseexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootLiquibaseExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
